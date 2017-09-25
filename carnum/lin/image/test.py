3b1e4c09ed4e39c75eb6b8b75063f33348ef66e9
lambsnoww <linxue@lx-2.local>
2017-09-25 09:39:35 +0800

    begin img processing

--- carnum/lin/image/test.py
+++ carnum/lin/image/test.py
@@ -7,16 +7,18 @@ Created on 2017年9月18日
 import numpy as np
 import cv2
 import os
+import pybrain
 
 dir = '../data/sample'
 imgPath = []
 for files in os.walk(dir):
 #    print files[0]
     for i in files[2]:
-#        print files[0] + '/' + i
         imgPath.append(files[0] + '/' + i)
 
 imgPath = imgPath[1:]
+for i in imgPath:
+    print i
         
         
 #for files in imgPath:
@@ -24,8 +26,28 @@ imgPath = imgPath[1:]
 #    cv2.namedWindow("车辆图像")
 #    cv2.imshow('车辆图', img)
 
-img = cv2.imread(imgPath[1], cv2.CV_LOAD_IMAGE_COLOR)
+print type(imgPath)
+print len(imgPath)
+
+img = cv2.imread(imgPath[9], cv2.CV_LOAD_IMAGE_COLOR)
 cv2.namedWindow("车辆图像")
 cv2.imshow('车辆图', img)
 cv2.waitKey()
+
+#########################
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.bitwise_not(gray)
+thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+
+
+
+
+
+
+
+
+
+
     
