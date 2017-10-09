0a59a799bdb04ff4c4e2a17a0970742c06689bbb
lambsnoww <linxue@lx-2.local>
2017-10-09 10:54:46 +0800

    initialized

--- carnum/lin/image/cartools/imgPreprocess.py
+++ carnum/lin/image/cartools/imgPreprocess.py
@@ -5,34 +5,43 @@ Created on 2017年9月26日
 @author: linxue
 '''
 import cv2
+import cv2.cv as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
+def preprocess(img_origin):
+    img = img_origin.copy()
 
-def preprocess(img):
-#高斯降噪 + 直方均衡
-
-#高斯降噪 + 直方均衡
-#blur = cv2.GaussianBlur(img, (5,5), 0)
-#blur = cv2.medianBlur(img, 0)#中值滤波对椒盐噪声的去噪效果最好
-
-#【滤波】双边滤波能在保持边界清晰的情况下有效的去除噪音。但是这种操作与其他滤波器相比会比较慢。
-    blur = cv2.bilateralFilter(img, 9, 75, 75)
-#【降噪】
+#【滤波/降噪】双边滤波能在保持边界清晰的情况下有效的去除噪音。但是这种操作与其他滤波器相比会比较慢。
+#    img = cv2.bilateralFilter(img, 9, 75, 75)
+#【滤波/降噪】
+#    img = cv2.GaussianBlur(img, (5,5), 0)
+    img = cv2.medianBlur(img, 0)#中值滤波对椒盐噪声的去噪效果最好
+    print "【中值滤波】\n"
+#【区域直方图均衡化】
     clahe = cv2.createCLAHE(clipLimit=2,tileGridSize=(10,10))
-    blurred = clahe.apply(blur)
-#【图像锐化】
+    img = clahe.apply(img)
+    print "【区域直方图均衡化】\n"
+#【边缘锐化】
+    img = cv2.Laplacian(img, 0, ksize = 3)
+    print "【边缘锐化】\n"
+    return img
 
-#【二值化】
-#【腐蚀】【膨胀】【开运算】【闭运算】
-#kernel = np.ones((5,5), np.uint8)
-#erosion = cv2.erode(blurred, kernel, iterations = 1)
-
-    cv2.imshow('blurred', blurred)
-    cv2.waitKey()
+def edgeDet(imgEdge):
+    imgMat = imgEdge.matrix
+    print imgMat
 
 if __name__ == '__main__':
-    img = cv2.imread('../123.jpg', 0)
-#    cv2.imshow('img', img)
-#    cv2.waitKey()
-    preprocess(img)
+    img = cv2.imread('../2.jpg', 0)
+    imgEdge = preprocess(img)
+    
+    imgEdge = cv2.resize(imgEdge, (imgEdge.shape[1]/2, imgEdge.shape[0]/2))
+    print imgEdge
+    cv2.imshow("", imgEdge)
+    cv2.waitKey()
+    
+    
+    
+    
+    
+    
